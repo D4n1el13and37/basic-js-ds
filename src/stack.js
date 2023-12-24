1fd6dbb8a0125c411b854824bd0d6942300ff52f
D4n1el13and37 <daniel837@mail.ru>
2023-12-24 16:44:47 +0300

    feat: compleate stack task

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const stackLength = this.stack.length;
+    if (stackLength === 0) {
+      return undefined;
+    } else {
+      return this.stack.pop(stackLength - 1);
+    }
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const stackLength = this.stack.length;
+    if (stackLength === 0) {
+      return undefined;
+    } else {
+      return this.stack[stackLength - 1];
+    }
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
